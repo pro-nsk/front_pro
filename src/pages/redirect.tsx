5de66d9a95f724a45c4aaa2639d94e9a1de5edc6
motors nsk <motors@live.ru>
2019-08-28 10:01:58 +0700

    post

--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -19,7 +19,8 @@ const existingPostMap = {
     181262466133: '/bc-manhetten',
     13420893787: '/ochistnye-sooruzhenija',
     181007759843: '/vechernij-ekb',
-    163979833023: '/berdskie-skaly'
+    163979833023: '/berdskie-skaly',
+    95899344453: '/300-let-sosna'
 }
 
 class Redirect extends React.Component<AppProps> {
