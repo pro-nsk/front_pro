f15b5a01f6bf5d7219590d3d4a6770b125911ff7
motors nsk <motors@live.ru>
2019-09-01 16:22:19 +0700

    post

--- src/pages/redirect.tsx
+++ src/pages/redirect.tsx
@@ -22,7 +22,9 @@ const existingPostMap = {
     163979833023: '/berdskie-skaly',
     95899344453: '/300-let-sosna',
     6252231070: '/pervaja-plenka-fed',
-    101064056203: '/prosrochka-fed-3'
+    101064056203: '/prosrochka-fed-3',
+    163256022028: '/minolta-a-sweet-2',
+    165753600643: '/plenka-fed-2'
 }
 
 class Redirect extends React.Component<AppProps> {
