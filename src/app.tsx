127d1d88f25e2847837ad8ea2178906928a6fa0a
motors nsk <motors@live.ru>
2019-09-28 22:42:47 +0700

    move redirect from react to nginx

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,9 @@ import * as React from 'react'
 import { Redirect, Route, Router, Switch } from 'react-router'
 import AppProps from './util/appProps'
 import CreatePost from './pages/createPost'
-import Posts from './pages/home'
+import Home from './pages/home'
 import Login from './pages/login'
 import Logout from './pages/logout'
-import RedirectPage from './pages/redirect'
 import Register from './pages/register'
 import EditPost from './pages/editPost'
 import ViewPost from './pages/viewPost'
@@ -15,16 +14,13 @@ class App extends React.Component<AppProps> {
         return (
             <Router history={this.props.history}>
                 <Switch>
-                    {/* route to old site */}
-                    <Route path="/post/:id" component={RedirectPage}/>
-                    {/* route to old site */}
                     <Route path="/create" component={CreatePost} />
                     <Route path="/edit/:id" component={EditPost} />
                     <Route path="/login" component={Login} />
                     <Route path="/logout" component={Logout} />
                     <Route path="/register" component={Register} />
                     <Route path="/:urlname" component={ViewPost} />
-                    <Route path="/" component={Posts} />
+                    <Route path="/" component={Home} />
                     <Redirect to="/" />
                 </Switch>
             </Router>
